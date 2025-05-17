9e65227058852eb2f32ca717b8a6ad79000173ce
fstanew <fstanew@gmail.com>
2025-05-17 13:54:17 +0200

    Pop & count

--- main.c
+++ main.c
@@ -7,15 +7,17 @@ int main()
 
   U64 playBitBoard = 0ULL;
 
-  printf("Start:\n\n");
-  PrintBitBoard(playBitBoard);
+  playBitBoard |=(1ULL<<SQ64(D2));
+  playBitBoard|=(1ULL<<SQ64(D3));
+  playBitBoard|=(1ULL<<SQ64(D4));
 
-  playBitBoard|=(1ULL<<SQ64(D2));
-
-  
- printf("D2 ADDED:\n\n");
-  PrintBitBoard(playBitBoard);
-
+ int sq64=0;
+ while(playBitBoard)
+ {
+    sq64=POP(&playBitBoard);
+    printf("popped:%d",sq64);
+    PrintBitBoard(playBitBoard);
+ }
 
 	return 0;
 }
